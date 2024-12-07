e3925e644cada60d96ed7cbd2f557a651cab8113
FajarArif-fjr <fajararif.fjr@gmail.com>
2024-12-07 13:39:20 +0700

    Test1

--- soal1/index.py
+++ soal1/index.py
@@ -1,6 +1,25 @@
 # start code here
-from custom_function.index import fungsiKustomku
+# from custom_function.index import fungsiKustomku
+
+def crange(a, b):
+    return range(a, b + 1)
+def printnln(a):
+    print(a, end='')
 
 def soal1(input):
-  fungsiKustomku(input)
-  pass # Tuliskan logic kalian di sini. Hapus 'pass dan fungsiKustomku'!
+  for k in crange(1, input):
+    for b in crange(1, input):
+      if b == k:
+        j = k * 2 - 1
+        printnln(j)
+      elif b == input -(k-1):
+        n = b * 2 - 2
+        printnln(n)
+      elif b > k and b < input -(k-1):
+        printnln("B")
+      elif b < k and b > input -(k-1):
+        printnln("A")
+      else:
+        printnln('_')
+    print()
+
