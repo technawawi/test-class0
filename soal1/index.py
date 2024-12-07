1c1562151648eb258b8aa2d7f69813b6b7951ea1
affansaragih <mchoirulaffan@gmail.com>
2024-12-07 14:20:28 +0700

    Ujian 1 - M Choirul Affan

--- soal1/index.py
+++ soal1/index.py
@@ -1,6 +1,22 @@
 # start code here
-from custom_function.index import fungsiKustomku
+from custom_function.index import crange, printnln
 
-def soal1(input):
-  fungsiKustomku(input)
-  pass # Tuliskan logic kalian di sini. Hapus 'pass dan fungsiKustomku'!
+def soal1 (input):
+  no = 1
+  for y in crange(no, input):
+    for x in crange(no, input):
+      odd = x * 2 - 1
+      even = x * 2 - 2
+      if x == y:
+        printnln(odd)
+      elif x == input - (y-1):
+        printnln(even)
+      elif x >= y and x <= input - (y-1):
+        printnln("B")
+      elif x <= y and x >= input - (y-1):
+        printnln("A")
+      else:
+        printnln(" ")
+    print()
+
+soal1(9)
--- soal2/index.py
+++ soal2/index.py
@@ -1,5 +1,18 @@
 # start code here
-from custom_function.index import fungsiKustomku
+# from custom_function.index import 
 
 def soal2(input):
-  pass # Tuliskan logic kalian di sini. Hapus 'pass'!
+    for x in range(input):
+        for y in range(input):
+            angka = min(x + 1, y + 1, input - x, input - y)
+            if angka == 2 or (4 <= x <= 6 and 4 <= y <= 6 and (x == 4 or x == 6 or y == 4 or y == 6)):
+                print('*', end='')  
+            elif x == 5 and y == 5:
+                print('5', end='')
+            elif angka == 4:
+                print(' ', end='')
+            else:
+                print(angka, end='')
+        print()
+
+soal2(9)
