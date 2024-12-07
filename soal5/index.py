f258dfc0a4ef41f60a77b49f9a146f8ae662dacb
NaufalAbdurrohman <NaufalAbdurrohman1@gmail.com>
2024-12-07 14:38:28 +0700

    ujian1

--- soal5/index.py
+++ soal5/index.py
@@ -1,5 +1,11 @@
 # start code here
-from custom_function.index import fungsiKustomku
+from custom_function.index import crange, printnln
 
 def soal5(input):
-  pass # Tuliskan logic kalian di sini. Hapus 'pass'!
+  for y in crange(1,input):
+    for x in crange(1,input):
+      if x == 4 or y == 4 or x == 1 or y == 1:
+        printnln("*")
+      else:
+        printnln(" ")
+    print()
